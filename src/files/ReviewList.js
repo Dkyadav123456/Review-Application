dc7e1d26d5b31f00123c069c1f439de932c5a259
Dharmendra Yadav <you@example.com>
2022-12-15 17:38:12 +0530

    Reviws-Final

---
+++ src/files/ReviewList.js
@@ -0,0 +1,50 @@
+const ReviewList = [
+  {
+    id: 1,
+    name: "Dharmendra",
+    rating: 3,
+    comment:
+      "Hello I am Dharmendra Kumar Yadav from Ahmendabad Gujrat I am student of BCA From Maulana mazharul Haque Arabic Persian University Patna",
+  },
+  {
+    id: 2,
+    name: "Kunal",
+    rating: 4,
+    comment: "Hello This is Kunal Babu from Ratanpura Siwan Bihar",
+  },
+  {
+    id: 3,
+    name: "Raj",
+    rating: 2,
+    comment:
+      "I have Knowledge About C C++ java Python React JavaScript NodeJs etc ",
+  },
+  {
+    id: 4,
+    name: "Sandeep kumar",
+    rating: 2,
+    comment: "This is Sandeep kumar from Jaipur Rajshthan",
+  },
+  {
+    id: 5,
+    name: "Adik Raj",
+    rating: 3,
+    comment:
+      "Hello i am adik raj from ratanpura Siwan Bihar He is student of 2nd ",
+  },
+  {
+    id: 6,
+    name: "Ram",
+    rating: 5,
+    comment: "Ram kumar This is Ram from Gujrat",
+  },
+  {
+    id: 7,
+    name: "Shilpa",
+    rating: 2,
+    comment:
+      "She is a girl from Maharajganj siwan bihar she is student of BCA last year from maulanam mazharul haque arabic & Persian university Patna",
+  },
+];
+
+export default ReviewList;
